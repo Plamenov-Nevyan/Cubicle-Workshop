0c68a71804a1d177028d82f59398a8d5a6554bb8
Nevyan Plamenov <100707694+Plamenov-Nevyan@users.noreply.github.com>
2022-05-29 17:56:53 +0300

    Adding Cubes implemented
    Add new item to database functionality implemented

--- controllers/cubeController.js
+++ controllers/cubeController.js
@@ -4,6 +4,10 @@ const services = require('../services/cubeServices')
 router.get('/create', (req, res) => {
     res.render('create')
 })
+router.post('/create', (req, res) => {
+    services.saveCube(req.body)
+    .then(resp => res.redirect('/'))
+})
 
 router.get('/details/:cubeId', (req, res) => {
     let cube = services.getSpecificCube(req.params.cubeId)
--- services/cubeServices.js
+++ services/cubeServices.js
@@ -1,10 +1,21 @@
 const cubes = require('../config/database.json')
+const path = require('path')
+const fs = require('fs/promises')
+const idGenerator = require('uniqid')
 
 const getAllCubes = () => cubes
 
 const getSpecificCube = (cubeId) => cubes.find(cube => cube.id == cubeId)
 
+const saveCube = (data) => {
+    data.id = idGenerator()
+    cubes.push(data)
+   return fs.writeFile(
+        path.resolve('config', 'database.json'), JSON.stringify(cubes, ``, 4), {encoding:'utf-8'})
+}
+
 module.exports = {
     getAllCubes,
-    getSpecificCube
+    getSpecificCube,
+    saveCube
 }
