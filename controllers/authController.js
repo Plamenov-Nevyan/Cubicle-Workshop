487b9bbc676b1126ca813aed4d93a3a7ab1a6919
Nevyan Plamenov <100707694+Plamenov-Nevyan@users.noreply.github.com>
2022-06-16 12:10:16 +0300

    Added login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,11 +3,26 @@ const {authServices} = require('../services/authServices')
 const jwt = require('jsonwebtoken')
 const {secret, cookieName} = require('../config/authConstants')
 
+
 router.get('/login', (req, res) => {
     try{
         res.render('loginPage')
     }catch(err){
         res.render('404', {status:'404 - Page Not Found', message:`${err.message}`})
+    }
+})
+router.post('/login', async (req, res) => {
+    let responseData = await authServices.checkIfPasswordExist(req.body)
+
+    if(typeof responseData == `string`){return res.render('404', {status:`401 - Unauthorized`, message:`${responseData}`})}
+    
+    if(responseData.isAuthenticated){
+        let token = jwt.sign({username:responseData.username, _id:responseData._id}, secret, {expiresIn:'2d'})
+        res.cookie(cookieName,token,{httpOnly:true})
+        res.redirect('/')
+    }
+    else{
+        return res.render('404', {status:`401 - Unauthorized`, message:`Password is incorrect!`})
     }
 })
 
@@ -20,14 +35,13 @@ router.get('/register', (req, res) => {
     
 })
 router.post('/register', (req, res) => {
-      authServices.checkIfUserExists(req.body)
+      authServices.checkIfUserExists(req.body.username)
       .then(async(result) => {
          if(!result){
             if(req.body.password !== req.body.repeatPassword){
                 return res.render('404', {status: `401-Unauthorized`, message:`Passwords must match each other!`})
             }
             let user = await authServices.registerUser(req.body)
-            console.log(user);
             let token = jwt.sign({username:user.username, _id: user._id}, secret, {expiresIn:'2d'})
             res.cookie(cookieName, token, {httpOnly:true})
             res.redirect('/')
--- services/authServices.js
+++ services/authServices.js
@@ -4,15 +4,30 @@ const {promisify} = require('util')
 const bcrypt = require('bcrypt')
 
 
-const checkIfUserExists = async (userData) => User.exists({username:userData.username}).exec()
+const checkIfUserExists = async (username) => User.exists({username}).exec()
 
 const registerUser = async (userData) => {
   let hashedPass = await bcrypt.hash(userData.password, saltRounds)
   return User.create({username:userData.username, password: hashedPass})
 }
 
+const checkIfPasswordExist = async (userData) => {
+    let user = await User.findOne({username: userData.username}).lean()
+    if(user){
+        let isAuthenticated = await bcrypt.compare(userData.password, user.password)
+        return {
+            user,
+            isAuthenticated
+        }
+    } 
+    else{
+        return `User does not exist!`
+    }
+}
+
 
 exports.authServices = {
     checkIfUserExists,
+    checkIfPasswordExist,
     registerUser
 }
